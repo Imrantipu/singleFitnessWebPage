aa5efe5313024064f19f54b11f4741157323008d
Imran <imrantipu90@yahoo.com>
2022-09-29 13:15:06 +0200

    building block made

---
+++ src/components/Buildingblock/Buildingblock.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Buildingblock.css'
+
+const Buildingblock = () => {
+    return (
+        <div className='buildingblock'>
+            <div>
+                
+            <h2>this is </h2>
+            </div>
+            <div>
+            <h2>this is </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Buildingblock;
