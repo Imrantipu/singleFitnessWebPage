4a4cac700d49a3f020564ebbf752fa2c8ac3f534
Imran <imrantipu90@yahoo.com>
2022-11-23 14:41:11 +0100

    json data created and leftside component made

---
+++ src/components/LeftSide/LeftSideTop/LeftSideTop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./LeftSideTop.css"
+
+const LeftSideTop = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LeftSideTop;
